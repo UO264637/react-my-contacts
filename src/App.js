63211810de006128a8da269ad8633269aae7ad30
UO264637 <uo264637@uniovi.es>
2023-04-27 20:30:14 +0200

    Guion hasta 4.7

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import CardList from './components/CardList';
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=20')
+    .then(response => response.json())
+     .then(contacts => setContacts(contacts.results));
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='tc '>
+      <header>
+        <h1 className='f1'>My contacts</h1>
       </header>
+      {contacts.length === 0 ? <h2 className='f2'>Loading...</h2> :
+      <CardList contacts={contacts}/>
+      }
+      <footer>
+        <hr/><p>Desarrollo de Software para Dispositivos Moviles. 
+          {new Date().getFullYear()}</p>
+      </footer>
     </div>
   );
 }
